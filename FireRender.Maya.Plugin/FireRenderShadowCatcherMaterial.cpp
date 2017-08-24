929c7eea4766074f42def34e1ea3b22e759c1a9b
Denis Biryukov <dbirukov@luxoft.com>
2017-08-24 11:26:23 +0300

    ShadowCatcher material interface

--- FireRender.Maya.Plugin/FireRenderShadowCatcherMaterial.cpp
+++ FireRender.Maya.Plugin/FireRenderShadowCatcherMaterial.cpp
@@ -34,6 +34,24 @@ namespace
 		MObject disableSwatch;
 		MObject swatchIterations;
 	}
+
+	template<class T> struct AttributeEntry
+	{
+		MObject *object;
+		std::string name;
+		std::string shortName;
+		T min;
+		T max;
+		T def;
+		MFnNumericData::Type type;
+	};
+	struct ColorAttributeEntry
+	{
+		MObject *object;
+		std::string name;
+		std::string shortName;
+		std::vector<float> def;
+	};
 }
 
 void* FireMaya::ShadowCatcherMaterial::creator()
@@ -44,53 +62,70 @@ void* FireMaya::ShadowCatcherMaterial::creator()
 MStatus FireMaya::ShadowCatcherMaterial::initialize()
 {
 	MFnNumericAttribute nAttr;
-	MFnTypedAttribute tAttr;
+	
+	std::vector<AttributeEntry<float>> floatAttributes =
+	{
+		{&Attribute::bgWeight, "bgWeight", "bgw", 0.0, 1.0, 1.0, MFnNumericData::kFloat},
+		{&Attribute::refWeight, "refWeight", "rw", 0.0, 1.0, 1.0, MFnNumericData::kFloat},
+		{&Attribute::shadowTransparency, "shadowTransp", "st", 0.0, 1.0, 0.0, MFnNumericData::kFloat},
+		{&Attribute::shadowWeight, "shadowWeight", "sw", 0.0, 1.0, 1.0, MFnNumericData::kFloat}
+	};
+	std::vector<AttributeEntry<bool>> boolAttributes = 
+	{
+		{ &Attribute::bgIsEnv, "bgIsEnv", "bgie", 0, 0, 1, MFnNumericData::kBoolean },
+		{ &Attribute::refOverride, "refOverride", "ro", 0, 0, 0, MFnNumericData::kBoolean },
+		{ &Attribute::useNormalMap, "useNormalMap", "unm", 0, 0, 0, MFnNumericData::kBoolean },
+		{ &Attribute::useDispMap, "useDispMap", "udm", 0, 0, 0, MFnNumericData::kBoolean },
+		{ &Attribute::disableSwatch, "disableSwatch", "ds", 0, 0, 0, MFnNumericData::kBoolean }
+	};
 
-	editorTemplate - l "Background is Environment" - addControl "bgIsEnv";
-	editorTemplate - l "Background weight" - addControl "bgWeight";
-	editorTemplate - l "Background color" - addControl "bgColor";
-
-	editorTemplate - l "Reflective override" - addControl "refOverride";
-	editorTemplate - l "Reflective weight" - addControl "refWeight";
-	editorTemplate - l "Reflective color" - addControl "refColor";
-
-	editorTemplate - l "Shadow color" - addControl "shadowColor";
-	editorTemplate - l "Shadow weight" - addControl "shadowWeight";
-	editorTemplate - l "Shadow transparency" - addControl "shadowTransp";
-
-	editorTemplate - l "Use normal map" - addControl "useNormalMap";
-	editorTemplate - l "Normal map" - addControl "normalMap";
-	editorTemplate - l "Use displacement map" - addControl "useDispMap";
-	editorTemplate - l "Displacement map" - addControl "dispMap";
-
-
-	Attribute::bgIsEnv = nAttr.create("bgIsEnv", "bgie", MFnNumericData::kBoolean, 0);
-	MAKE_INPUT(nAttr);
-
-	Attribute::bgColor = nAttr.createColor("bgColor", "bgc");
-	MAKE_INPUT(nAttr);
-	nAttr.setDefault(0.0, 0.0, 0.0);
+	std::vector<ColorAttributeEntry> colorAttributes = 
+	{
+		{&Attribute::bgColor, "bgColor", "bgc", {0.0f, 0.0f, 0.0f} },
+		{&Attribute::refColor, "refColor", "rc", { 0.0f, 0.0f, 0.0f } },
+		{&Attribute::shadowColor, "shadowColor", "sc", { 0.0f, 0.0f, 0.0f } },
+		{&Attribute::normalMap, "normalMap", "nm", { 0.0f, 0.0f, 0.0f } },
+		{&Attribute::dispMap, "dispMap", "dm", { 0.0f, 0.0f, 0.0f } }
+	};
 
 	Attribute::output = nAttr.createColor("outColor", "oc");
 	MAKE_OUTPUT(nAttr);
-
-	Attribute::disableSwatch = nAttr.create("disableSwatch", "ds", MFnNumericData::kBoolean, 0);
-	MAKE_INPUT(nAttr);
 
 	Attribute::swatchIterations = nAttr.create("swatchIterations", "swi", MFnNumericData::kInt, 4);
 	MAKE_INPUT(nAttr);
 	nAttr.setMin(1);
 	nAttr.setMax(64);
 
-	CHECK_MSTATUS(addAttribute(Attribute::bgIsEnv));
-	CHECK_MSTATUS(addAttribute(Attribute::bgColor));
 	CHECK_MSTATUS(addAttribute(Attribute::output));
-
-	CHECK_MSTATUS(attributeAffects(Attribute::bgIsEnv, Attribute::output));
-	CHECK_MSTATUS(attributeAffects(Attribute::bgColor, Attribute::output));
-
-	CHECK_MSTATUS(addAttribute(Attribute::disableSwatch));
 	CHECK_MSTATUS(addAttribute(Attribute::swatchIterations));
+	
+	for (auto &att : floatAttributes)
+	{
+		*(att.object) = nAttr.create(att.name.c_str(), att.shortName.c_str(), att.type, att.def);
+		MAKE_INPUT(nAttr);
+		nAttr.setDefault(att.def);
+		nAttr.setMin(att.min);
+		nAttr.setMax(att.max);
+		CHECK_MSTATUS(addAttribute(*(att.object)));
+		CHECK_MSTATUS(attributeAffects(*(att.object), Attribute::output));
+	}
+
+	for (auto &att : boolAttributes)
+	{
+		*(att.object) = nAttr.create(att.name.c_str(), att.shortName.c_str(), att.type, att.def);
+		MAKE_INPUT(nAttr);
+		CHECK_MSTATUS(addAttribute(*(att.object)));
+		CHECK_MSTATUS(attributeAffects(*(att.object), Attribute::output));
+	}
+
+	for (auto &att : colorAttributes)
+	{
+		*(att.object) = nAttr.createColor(att.name.c_str(), att.shortName.c_str());
+		MAKE_INPUT(nAttr);
+		nAttr.setDefault(att.def[0], att.def[1], att.def[2]);
+		CHECK_MSTATUS(addAttribute(*(att.object)));
+		CHECK_MSTATUS(attributeAffects(*(att.object), Attribute::output));
+	}
 
 	return MS::kSuccess;
 }
