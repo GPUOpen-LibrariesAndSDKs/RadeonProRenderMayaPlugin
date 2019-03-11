11bb580522d07f06f9def7efa67b3290da380330
tnarine-amd <tracy.narine@amd.com>
2019-03-11 09:46:17 -0400

    Dev/update/maya2019 (#213)
    * Adding support for Maya 2019 on Windows.
    
    * More updates for Maya 2019 support.
    
    * Initial support for Maya 2019 macOS.
    
    * Fixing post script call for Debug.
    
    * Fixing the install of the rpr.mod for Maya 2019.
    
    * Update xc file

--- FireRender.Maya.OSX/sh/uninstall.sh
+++ FireRender.Maya.OSX/sh/uninstall.sh
@@ -9,16 +9,21 @@ fi
 
 rm -f /Users/Shared/Autodesk/modules/maya/2016/rpr.mod
 rm -f /Users/Shared/Autodesk/modules/maya/2017/rpr.mod
+rm -f /Users/Shared/Autodesk/modules/maya/2018/rpr.mod
+rm -f /Users/Shared/Autodesk/modules/maya/2019/rpr.mod
 
 localUsers=$( dscl . list /Users UniqueID | awk '$2 >= 501 {print $1}' | grep -v admin )
 
 for userName in $localUsers; do
     echo "Removing shelves for $userName..."
-    if [ -d "/Users/$userName/Library/Preferences/Autodesk/maya/2016/prefs/shelves/" ]; then
-        rm -f "/Users/$userName/Library/Preferences/Autodesk/maya/2016/prefs/shelves/shelf_Radeon_ProRender.mel"
-    fi
     if [ -d "/Users/$userName/Library/Preferences/Autodesk/maya/2017/prefs/shelves/" ]; then
         rm -f "/Users/$userName/Library/Preferences/Autodesk/maya/2017/prefs/shelves/shelf_Radeon_ProRender.mel"
+    fi
+    if [ -d "/Users/$userName/Library/Preferences/Autodesk/maya/2018/prefs/shelves/" ]; then
+        rm -f "/Users/$userName/Library/Preferences/Autodesk/maya/2018/prefs/shelves/shelf_Radeon_ProRender.mel"
+    fi
+    if [ -d "/Users/$userName/Library/Preferences/Autodesk/maya/2019/prefs/shelves/" ]; then
+        rm -f "/Users/$userName/Library/Preferences/Autodesk/maya/2019/prefs/shelves/shelf_Radeon_ProRender.mel"
     fi
 done
 
