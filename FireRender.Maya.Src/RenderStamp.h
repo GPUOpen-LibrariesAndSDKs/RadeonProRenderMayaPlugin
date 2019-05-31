5bfbe7cff4308c3a776f07818afa1da4dc7cc03e
bsavery <brian.savery@gmail.com>
2019-05-31 08:51:54 -0700

    Revert "Increase cancellation checking frequency for MacOS"

--- FireRender.Maya.Src/RenderStamp.h
+++ FireRender.Maya.Src/RenderStamp.h
@@ -9,8 +9,21 @@ namespace FireMaya
 {
 	class RenderStamp
 	{
+	private:
+		int	m_renderDevice;
+		std::string m_friendlyUsedGPUName;
+		std::string m_cpuName;
+		std::string m_computerName;
+
 	public:
 		void AddRenderStamp(FireRenderContext& context, RV_PIXEL* pixels, int width, int height, bool flip, const char* format);
+
+	private:
+		std::string FormatRenderStamp(FireRenderContext& context, const char* format);
+		std::string GetCPUNameString();
+		std::string GetFriendlyUsedGPUName();
+		std::string GetComputerNameString();
+		int GetRenderDevice();
 	};
 
 }
