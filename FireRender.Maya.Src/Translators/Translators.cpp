bdabd19cef82df6ac1d065afb9c2195c80cae495
Vsevolod1983 <vgolovko@luxoft.com>
2019-08-26 18:18:31 +0300

    use just transform X-scale factor to x-flip the image instead of operating with pixels ithemselves

--- FireRender.Maya.Src/Translators/Translators.cpp
+++ FireRender.Maya.Src/Translators/Translators.cpp
@@ -607,6 +607,12 @@ namespace FireMaya
 		MMatrix scaleM;
 		scaleM.setToIdentity();
 		scaleM[0][0] = scaleM[1][1] = scaleM[2][2] = 0.01;
+
+		if (!IsFlipIBL())
+		{
+			scaleM[0][0] = -scaleM[0][0];
+		}
+
 		m *= scaleM;
 		float mfloats[4][4];
 		m.get(mfloats);
