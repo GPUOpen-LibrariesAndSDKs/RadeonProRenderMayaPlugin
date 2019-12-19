dd5a65d93b33ad94a6dee4ec75db247efec49fbd
Aleksandr Gurov <s1lent.ssh@gmail.com>
2019-12-19 17:24:09 +0300

    RPRMAYA-1565 Changed order of blending for blend node (#381)
    Order of blending for blend node is reversed to match maya behavior

--- FireRender.Maya.Src/MayaStandardNodesSupport/BlendColorsConverter.cpp
+++ FireRender.Maya.Src/MayaStandardNodesSupport/BlendColorsConverter.cpp
@@ -7,9 +7,10 @@ MayaStandardNodeConverters::BlendColorsConverter::BlendColorsConverter(const Con
 
 frw::Value MayaStandardNodeConverters::BlendColorsConverter::Convert() const
 {
-	auto color1 = m_params.scope.GetValueWithCheck(m_params.shaderNode, "color1");
-	auto color2 = m_params.scope.GetValueWithCheck(m_params.shaderNode, "color2");
-	auto blender = m_params.scope.GetValueWithCheck(m_params.shaderNode, "blender");
+	frw::Value color1 = m_params.scope.GetValueWithCheck(m_params.shaderNode, "color1");
+	frw::Value color2 = m_params.scope.GetValueWithCheck(m_params.shaderNode, "color2");
+	frw::Value blender = m_params.scope.GetValueWithCheck(m_params.shaderNode, "blender");
 
-	return m_params.scope.MaterialSystem().ValueBlend(color1, color2, blender);
+	// Reverse order to match maya hardware render behavior
+	return m_params.scope.MaterialSystem().ValueBlend(color2, color1, blender);
 }
