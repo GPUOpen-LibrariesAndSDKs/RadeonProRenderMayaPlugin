69dd154cc80ec504ffcdd97168bced08666603c3
Aleksandr Gurov <s1lent.ssh@gmail.com>
2019-12-20 18:41:04 +0300

    RPRMAYA-1570 Integrated bump2d node (#386)
    Support for maya Bump2d Node added

--- FireRender.Maya.Src/MayaStandardNodesSupport/Bump2dConverter.cpp
+++ FireRender.Maya.Src/MayaStandardNodesSupport/Bump2dConverter.cpp
@@ -1,5 +1,7 @@
 #include "Bump2dConverter.h"
 #include "FireMaya.h"
+#include "FireRenderMath.h"
+#include <maya/MPlugArray.h>
 
 MayaStandardNodeConverters::Bump2dConverter::Bump2dConverter(const ConverterParams& params) : BaseConverter(params)
 {
@@ -7,11 +9,79 @@ MayaStandardNodeConverters::Bump2dConverter::Bump2dConverter(const ConverterPara
 
 frw::Value MayaStandardNodeConverters::Bump2dConverter::Convert() const
 {
-	if (auto bumpValue = m_params.scope.GetConnectedValue(m_params.shaderNode.findPlug("bumpValue")))
+	frw::Value bumpDepth = m_params.scope.GetValue(m_params.shaderNode.findPlug("bumpDepth"));
+	UseAs useAs = static_cast<UseAs>(m_params.shaderNode.findPlug("bumpInterp").asInt());
+
+	switch (useAs)
 	{
-		// TODO: need to find a way to turn generic input color data
-		// into bump/normal format
+		case UseAs::Bump: return GetBumpValue(bumpDepth);
+		case UseAs::TangentSpaceNormals: return GetNormalValue(bumpDepth);
+		case UseAs::ObjectSpaceNormals: return {};
+		default: assert(false);
 	}
 
-	return nullptr;
+	return frw::Value();
+}
+
+frw::Value MayaStandardNodeConverters::Bump2dConverter::GetBumpValue(const frw::Value& strength) const
+{
+	frw::Value color = m_params.scope.GetConnectedValue(m_params.shaderNode.findPlug("bumpValue"));
+
+	if (!color)
+	{
+		return frw::Value();
+	}
+
+	frw::BumpMapNode imageNode(m_params.scope.MaterialSystem());
+	imageNode.SetMap(color);
+
+	if (strength != 1.0f)
+	{
+		imageNode.SetValue("bumpscale", strength);
+	}
+
+	return imageNode;
+}
+
+
+frw::Value MayaStandardNodeConverters::Bump2dConverter::GetNormalValue(const frw::Value& strength) const
+{
+	frw::Value color;
+
+	// Normal node should expect float3 as input but this node could have only float as input
+	// In maya, if this node connected directly to file node, visually there's connection between outAlpha and bumpValue
+	// But under hood it retieves outColor from file node
+	MPlug bumpValuePlug = m_params.shaderNode.findPlug("bumpValue");
+	MPlugArray connections;
+	bumpValuePlug.connectedTo(connections, true, false);
+
+	if (connections.length() != 1)
+	{
+		return frw::Value();
+	}
+
+	std::vector<MPlug> dest;
+	dest.resize(connections.length());
+	connections.get(dest.data());
+
+	if (dest.at(0).node().hasFn(MFn::kFileTexture))
+	{
+		// "oc" stands for "outColor"
+		color = m_params.scope.GetValue(dest.at(0).node(), "oc");
+	}
+
+	if (!color)
+	{
+		return frw::Value();
+	}
+
+	frw::NormalMapNode imageNode(m_params.scope.MaterialSystem());
+	imageNode.SetMap(color);
+
+	if (strength != 1.0f)
+	{
+		imageNode.SetValue("bumpscale", strength);
+	}
+
+	return imageNode;
 }
--- FireRender.Maya.Src/MayaStandardNodesSupport/Bump2dConverter.h
+++ FireRender.Maya.Src/MayaStandardNodesSupport/Bump2dConverter.h
@@ -7,9 +7,21 @@ namespace MayaStandardNodeConverters
 
 	class Bump2dConverter : public BaseConverter
 	{
+
+		enum class UseAs
+		{
+			Bump = 0,
+			TangentSpaceNormals,
+			ObjectSpaceNormals
+		};
+
 	public:
 		Bump2dConverter(const ConverterParams& params);
 		virtual frw::Value Convert() const override;
+
+	private:
+		frw::Value GetBumpValue(const frw::Value& strength) const;
+		frw::Value GetNormalValue(const frw::Value& strength) const;
 	};
 
 }
