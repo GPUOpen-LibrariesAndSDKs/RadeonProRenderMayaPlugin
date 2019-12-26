ad3515173b3d5941e90dba0bb49b7d98f7edca67
Aleksandr Gurov <s1lent.ssh@gmail.com>
2019-12-26 15:49:02 +0300

    Refactored plus minus average node (#392)
    Refactored plus minus average node.

--- FireRender.Maya.Src/MayaStandardNodesSupport/PlusMinusAverageConverter.h
+++ FireRender.Maya.Src/MayaStandardNodesSupport/PlusMinusAverageConverter.h
@@ -7,12 +7,24 @@ namespace MayaStandardNodeConverters
 
 	class PlusMinusAverageConverter : public BaseConverter
 	{
+
+		enum class Operation
+		{
+			None = 0,
+			Sum,
+			Subtract,
+			Average,
+		};
+
 	public:
 		PlusMinusAverageConverter(const ConverterParams& params);
 		virtual frw::Value Convert() const override;
 	private:
-		void GetArrayOfValues(const MFnDependencyNode& node, const char* plugName, std::vector<frw::Value>& arr) const;
-		frw::Value CalcElements(const std::vector<frw::Value>& arr, bool substract = false) const;
+		std::vector<frw::Value> GetArrayOfValues(const MFnDependencyNode& node, const std::string& plugName) const;
+
+		frw::Value ValuesSum(const std::vector<frw::Value>& values) const;
+		frw::Value ValuesSubtract(const std::vector<frw::Value>& values) const;
+		frw::Value ValuesAverage(const std::vector<frw::Value>& values) const;
 	};
 
 }
