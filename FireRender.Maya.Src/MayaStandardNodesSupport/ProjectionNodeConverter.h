f957cea0dea0acb3a433287582c1020b203f8ec0
Aleksandr Gurov <s1lent.ssh@gmail.com>
2019-12-26 22:05:00 +0300

    WIP

--- FireRender.Maya.Src/MayaStandardNodesSupport/ProjectionNodeConverter.h
+++ FireRender.Maya.Src/MayaStandardNodesSupport/ProjectionNodeConverter.h
@@ -8,6 +8,14 @@ namespace MayaStandardNodeConverters
 	class ProjectionNodeConverter : public BaseConverter
 	{
 
+		struct TransformInfo
+		{
+			frw::Value origin;
+			frw::Value rotationX;
+			frw::Value rotationZ;
+			frw::Value scale;
+		};
+
 		enum class ProjectionType
 		{
 			Off,
@@ -25,10 +33,9 @@ namespace MayaStandardNodeConverters
 		ProjectionNodeConverter(const ConverterParams& params);
 		virtual frw::Value Convert() const override;
 
-	private:		
-		frw::Value GetCameraOrigin(ProjectionType projectionType) const;
-		frw::Value GetCameraScale(ProjectionType projectionType) const;
-		std::pair<frw::Value, frw::Value> GetCameraRotation(ProjectionType projectionType) const;
+	private:	
+		TransformInfo GetCameraTransform() const;
+		TransformInfo GetPlaceTextureTransfrom() const;
 
 		std::unique_ptr<frw::BaseUVNode> CreateUVNode(ProjectionType projectionType) const;
 		frw::Image GetImageFromConnectedFileNode() const;
