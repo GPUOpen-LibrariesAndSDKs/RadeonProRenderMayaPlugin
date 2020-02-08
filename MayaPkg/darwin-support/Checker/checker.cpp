2f9a6af0cc62c1535e803e830cf15368e168ead7
Vsevolod Golovko <vgolovko@luxoft.com>
2020-02-08 04:24:29 +0300

    formselect removed to avoid hardware check to launch (#412)
    PURPOSE:
    We have OpenCL mention in formSelect.cpp file

--- MayaPkg/darwin-support/Checker/checker.cpp
+++ MayaPkg/darwin-support/Checker/checker.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 #include <fstream>
 #include <string>
-#include "formSelect.h"
 
 using namespace std;
 
@@ -12,7 +11,4 @@ int main(int argc, const char *argv[])
 {
 	outmsg = &cout;
 	logfile.open("log.txt");
-
-	formSelect fS=formSelect();
-	fS.formSelect_Shown();
 }
