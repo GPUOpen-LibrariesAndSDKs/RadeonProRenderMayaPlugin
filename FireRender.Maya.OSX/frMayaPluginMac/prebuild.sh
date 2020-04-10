66ba2a7f5bffde9b79f45a81b43a96bdca3b0c93
Vsevolod Golovko <vgolovko@luxoft.com>
2020-04-10 20:29:17 +0300

    PreBuild step haven't been launching (#448)
    PURPOSE:
    Cleanup before build is started haven't been performing properly for Mac builds
    It usually did not affect anything but it might.

--- FireRender.Maya.OSX/frMayaPluginMac/prebuild.sh
+++ FireRender.Maya.OSX/frMayaPluginMac/prebuild.sh
@@ -12,9 +12,8 @@
 # limitations under the License.
 
 # clear old folders
-
 pushd "/Users/Shared/RadeonProRender"
-rm -rf "cache icons image lib plug-ins renderDesc scripts shaders shelves modules"
+rm -rf cache icons image lib plug-ins renderDesc scripts shaders shelves modules
 popd
 
 mkdir -p "/Users/Shared/RadeonProRender"
