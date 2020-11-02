195e4ada715bcbadcbb3add7c3e12e76688114bb
Vsevolod Golovko <vgolovko@luxoft.com>
2020-11-02 19:58:51 +0300

    Fix for studio 2019 - in case if both studios are prsent (2017 and 2019) we shoudl still use 2017 (#104)

--- vs_path.bat
+++ vs_path.bat
@@ -13,8 +13,12 @@ if not exist %VsWhere% goto :eof
 
 set vs_ver=""
 
-for /f "usebackq tokens=1* delims=: " %%i in (`%VsWhere% -latest -requires Microsoft.VisualStudio.Component.VC.140`) do (
+%VsWhere% -version [14.0,16.0) -latest -requires Microsoft.VisualStudio.Component.VC.140 >temp_maya_inst_studios
+
+for /f "usebackq tokens=1* delims=: " %%i in (temp_maya_inst_studios) do (
 	if /i "%%i"=="installationVersion" set vs_ver=%%j
 
 	if /i "%%i"=="installationPath" set vs_dir=%%j
 )
+
+del temp_maya_inst_studios
