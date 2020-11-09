2d2a658163e54c6fc0a3d954aafafec729c11a4f
DMotorygin <d_motorygin@yahoo.com>
2020-11-09 20:06:20 +0300

    ocio support (#108)
    PURPOSE: Enable ocio color management support
    
    EFFECT FOR THE USER: Use can set OCIO config and files from Maya preferences UI

--- FireRender.Maya.Src/Context/TahoeContext.cpp
+++ FireRender.Maya.Src/Context/TahoeContext.cpp
@@ -11,6 +11,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 ********************************************************************/
 #include "TahoeContext.h"
+#include "maya/MColorManagementUtilities.h"
 
 TahoeContext::LoadedPluginMap TahoeContext::m_gLoadedPluginsIDsMap;
 
@@ -248,6 +249,39 @@ void TahoeContext::setupContext(const FireRenderGlobalsData& fireRenderGlobalsDa
 	checkStatus(frstatus);
 	
 	updateTonemapping(fireRenderGlobalsData, disableWhiteBalance);
+
+	// OCIO
+	{
+		MStatus colorManagementStatus;
+		int isColorManagementOn = 0;
+		colorManagementStatus = MGlobal::executeCommand(MString("colorManagementPrefs -q -cmEnabled;"), isColorManagementOn);
+
+		int isConfigFileEnable = 0;
+		colorManagementStatus = MGlobal::executeCommand(MString("colorManagementPrefs -q -cmConfigFileEnabled;"), isConfigFileEnable);
+
+		if ((isColorManagementOn > 0) && (isConfigFileEnable > 0))
+		{
+			MString configFilePath;
+			colorManagementStatus = MGlobal::executeCommand(MString("colorManagementPrefs -q -cfp;"), configFilePath);
+
+			MString renderingSpaceName;
+			colorManagementStatus = MGlobal::executeCommand(MString("colorManagementPrefs -q -rsn;"), renderingSpaceName);
+
+			frstatus = rprContextSetParameterByKeyString(frcontext, RPR_CONTEXT_OCIO_CONFIG_PATH, configFilePath.asChar());
+			checkStatus(frstatus);
+
+			frstatus = rprContextSetParameterByKeyString(frcontext, RPR_CONTEXT_OCIO_RENDERING_COLOR_SPACE, renderingSpaceName.asChar());
+			checkStatus(frstatus);
+		}
+		else
+		{
+			frstatus = rprContextSetParameterByKeyString(frcontext, RPR_CONTEXT_OCIO_CONFIG_PATH, "");
+			checkStatus(frstatus);
+
+			frstatus = rprContextSetParameterByKeyString(frcontext, RPR_CONTEXT_OCIO_RENDERING_COLOR_SPACE, "");
+			checkStatus(frstatus);
+		}
+	}
 }
 
 void TahoeContext::updateTonemapping(const FireRenderGlobalsData& fireRenderGlobalsData, bool disableWhiteBalance)
