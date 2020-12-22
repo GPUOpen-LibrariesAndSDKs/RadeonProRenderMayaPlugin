4d07f2b64a53dc6864e58c2b12495c64ed50f713
DMotorygin <d_motorygin@yahoo.com>
2020-12-23 01:37:44 +0300

    add anim ex flag (#139)
    PURPOSE: Currently export UI is unclear
    
    EFFECT FOR THE USER: Added option to export animation without exporting each frame flag and flags naming now better describes their function.

--- FireRender.Maya.Src/FireRenderExportCmd.cpp
+++ FireRender.Maya.Src/FireRenderExportCmd.cpp
@@ -100,7 +100,7 @@ bool SaveExportConfig(const std::wstring& filePath, TahoeContext& ctx, const std
 
 	json << "\"output\" : " << "\"" << fileName.c_str() << ".png\",\n";
 
-	json << "\"output.json\" : " << "\"" << configName.c_str() << "\",\n";
+	json << "\"output.json\" : \"output.json\",\n";
 
 	// write file data fields
 	json << "\"width\" : " << ctx.width() << ",\n";
