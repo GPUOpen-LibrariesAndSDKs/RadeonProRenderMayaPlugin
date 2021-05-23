3fe2725ca9adab0420c3ccd6cb1e1cb408802b14
Vsevolod Golovko <vgolovko@luxoft.com>
2021-05-23 23:50:51 +0300

    RPRMAYA-2760 (#204)
    PURPOSE:
    Deformation motion blur makes Batch render hung.
    
    EFFECT OF CHANGES:
    Deformation motion blur works in Batch render mode

--- FireRender.Maya.Src/FireRenderObjects.cpp
+++ FireRender.Maya.Src/FireRenderObjects.cpp
@@ -1520,7 +1520,13 @@ void FireRenderMesh::GetShapes(std::vector<frw::Shape>& outShapes)
 	{
 		bool deformationMotionBlurEnabled = IsMotionBlurEnabled(MFnDagNode(dagPath.node())) && TahoeContext::IsGivenContextRPR2(context) && !context->isInteractive();
 		unsigned int motionSamplesCount = deformationMotionBlurEnabled ? context->motionSamples() : 0;
-		outShapes = FireMaya::MeshTranslator::TranslateMesh(context->GetContext(), Object(), motionSamplesCount, dagPath.fullPathName());
+
+		//Ignore set objects dirty calls while creating a mesh, because it moght lead to infinite lookps in case if deformtion motion blur is used
+		{
+			ContextSetDirtyObjectAutoLocker locker(*context);
+			outShapes = FireMaya::MeshTranslator::TranslateMesh(context->GetContext(), Object(), motionSamplesCount, dagPath.fullPathName());
+		}
+
 		m.isMainInstance = true;
 		context->AddMainMesh(this);
 	}
