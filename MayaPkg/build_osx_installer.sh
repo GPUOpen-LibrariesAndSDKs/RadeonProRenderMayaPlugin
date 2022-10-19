17c7745f2eab2755bae2be46bb8c861285c8215a
DMotorygin <d_motorygin@yahoo.com>
2022-10-19 22:44:26 +0200

    Add maya 2023 support (#333)
    PURPOSE: Add Maya 2023 support and remove Maya 2019

--- MayaPkg/build_osx_installer.sh
+++ MayaPkg/build_osx_installer.sh
@@ -13,19 +13,9 @@
 
 pushd ../FireRender.Maya.OSX/frMayaPluginMac/frMayaMac
 
-xcodebuild -scheme "Release 2019" -project ./RadeonProRender.xcodeproj clean
 xcodebuild -scheme "Release 2020" -project ./RadeonProRender.xcodeproj clean
 xcodebuild -scheme "Release 2022" -project ./RadeonProRender.xcodeproj clean
-
-xcodebuild -quiet -scheme "Release 2019" -project ./RadeonProRender.xcodeproj -UseModernBuildSystem=NO
-if test $? -eq 0
-then
-	echo "Release 2019 built successfully"
-else
-	echo "Release 2019 failed to build"
-	popd
-	exit 1	
-fi
+xcodebuild -scheme "Release 2023" -project ./RadeonProRender.xcodeproj clean
 
 xcodebuild -quiet -scheme "Release 2020" -project ./RadeonProRender.xcodeproj -UseModernBuildSystem=NO
 if test $? -eq 0
@@ -47,6 +37,16 @@ else
 	exit 1
 fi
 
+xcodebuild -quiet -scheme "Release 2023" -project ./RadeonProRender.xcodeproj -UseModernBuildSystem=NO
+if test $? -eq 0
+then
+	echo "Release 2023 built successfully"
+else
+	echo "Release 2023 failed to build"
+	popd
+	exit 1
+fi
+
 popd
 
 rm -rf ./darwin-support/Checker/.build
@@ -55,6 +55,6 @@ python3 build_osx_installer.py  --nomatlib  $1
 
 # Clean the dev modules to enable testing locally
 
-rm -f /Users/Shared/Autodesk/modules/maya/2019/rpr.mod
 rm -f /Users/Shared/Autodesk/modules/maya/2020/rpr.mod
 rm -f /Users/Shared/Autodesk/modules/maya/2022/rpr.mod
+rm -f /Users/Shared/Autodesk/modules/maya/2023/rpr.mod
