91dda8205cd3364c197e62e5de9533253f22267f
DMotor <d_motorygin@yahoo.com>
2023-09-11 22:12:23 +0200

    save correct buffer

--- FireRender.Maya.Src/FireRenderProduction.cpp
+++ FireRender.Maya.Src/FireRenderProduction.cpp
@@ -1095,7 +1095,7 @@ void FireRenderProduction::RenderTiles()
 		RenderViewUpdater::UpdateAndRefreshRegion(data, m_width, m_height, RenderRegion(0, m_width - 1, m_height - 1, 0));
 	});
 
-	outBuffers.clear();
+	//outBuffers.clear();
 
 	UploadAthenaData();
 }
